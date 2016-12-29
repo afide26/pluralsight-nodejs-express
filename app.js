2a140360e6eec6dc33e3dfbd4063e5fc4d6325ca
Alan Fidelino <afidedev26@gmail.com>
2016-12-29 22:43:06 +1100

    Use EJS as templating engine

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var app = express();
 var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/src/views'));
+// app.use(express.static(__dirname + '/src/views'));
+app.set('views', (__dirname + '/src/views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-  res.send('Hello from Express');
+  res.render('index', {title: 'Hello from EJS', list:['a','b']});
 });
 
 app.get('/books', function(req, res){
